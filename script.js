a64e7f09649bd88b1e6bfad735b011fe32798500
BryanYHuang <106937552+BryanYHuang@users.noreply.github.com>
2026-04-14 20:53:49 +0000

    Added ability to add information into boxes, created array in which information is deposited (I still need to make a three-columned container to make the information visible to the user)

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// add javascript here
+//let username = prompt("Enter your name:"); 
+
+const infoArray = ["Information", "Importance", "Due Date"]; 
+document.getElementById("addBtn").addEventListener("click", function(){
+    let dueDate = document.getElementById("dueDate").value; 
+    infoArray[0] = dueDate; 
+    let details = document.getElementById("details").value; 
+    infoArray[1] = details; 
+    let importance = document.querySelector('input[name="importance"]:checked').value;
+    infoArray[2] = importance;
+    console.log(infoArray); 
+}); 
+
+// function addInfo(){
+//     infoArray[0] = document.getElementsById("dueDate"); 
+//     infoArray[1] = document.getElementsByName("importance"); 
+//     infoArray[2] = document.getElementsById("details"); 
+// }
